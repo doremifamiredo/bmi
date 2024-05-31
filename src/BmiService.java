e62e953ab7f4a58255d871b3598dfa08ec3e1b25
Oleg Somov <doremi@fasollasi.ru>
2024-05-31 16:40:04 +0300

    Math.pow

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
     public int body(int kg, double m) {
-        double m2 = m * m;
+        // double m2 = m * m;
+        double m2 = Math.pow(m, 2);
         double index = kg / m2;
         return (int) index;
     }
